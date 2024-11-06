5cb17c8ac31ae005b61244ae158930ff4c064f6a
My Tuyen Phan <mytuyenp@gmail.com>
2024-11-05 22:45:00 -0600

    removed unused path

--- phaser-react/src/main.jsx
+++ phaser-react/src/main.jsx
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 
 import App from './App.jsx';
-import About from './routes/About.jsx'
+import About from './routes/About.jsx';
 import Survey from './routes/Survey.jsx';
-import Form from './routes/Form.jsx';
 
 const router = createBrowserRouter([
     {
@@ -15,7 +14,6 @@ const router = createBrowserRouter([
     },
     { path: "/about", element: <About />},
     { path: "/survey", element: <Survey />},
-    { path: "/form", element: <Form />},
 
 
 ]);
