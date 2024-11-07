609a8084ca57216ed5cbe0528eed5680d095a160
My Tuyen Phan <mytuyenp@gmail.com>
2024-11-07 00:00:13 -0600

    uses local storage to store counter of how many times player starts the game

--- phaser-react/src/states/flow1/Catalyst.jsx
+++ phaser-react/src/states/flow1/Catalyst.jsx
@@ -1,14 +1,30 @@
-//import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function Catalyst({ state, send }) {
-    
+    const [counter, setCounter] = useState(() => {
+      // Retrieve counter from localStorage or default to 0
+      return parseInt(localStorage.getItem('gameStartCounter')) || 0;
+    });
+  
+    useEffect(() => {
+      // Save the counter to localStorage whenever it changes
+      localStorage.setItem('gameStartCounter', counter);
+    }, [counter]);
+
+  const handleContinue = () => {
+    if (state.matches('catalyst_earth')) {
+      setCounter(prevCount => prevCount + 1);
+      console.log(counter);
+    }
+    send({type: "CONTINUE"});
+  };
 
   if (state.matches('catalyst_earth')) {
     return (
       <div>
         <h1> Catalyst Earth </h1>
         <p> Political corruption and rumors of fraud in the most recent election has cause much social unrest in Costa Rica. There are protests daily, and they often end in violence. Rivaling cartels are using the circumstances to gain political power and control over more territory. </p>
-        <button onClick={() => send({type: "CONTINUE"})}> Continue</button>
+        <button onClick={handleContinue}> Continue</button>
       </div>
     );
   }
