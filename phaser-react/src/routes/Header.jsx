08adba08fccb1689c5b3851cf9dfa58fdfd27fec
My Tuyen Phan <mytuyenp@gmail.com>
2024-11-07 14:49:23 -0600

    styled header component

--- phaser-react/src/routes/Header.jsx
+++ phaser-react/src/routes/Header.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from 'react';
+import {Link } from 'react-router-dom';
+import "./Header.css"
 
 export default function Header() {
+
   return (
-    <div>Header</div>
+    <header>
+            <div id="header-title">
+              <Link to="/">
+                {/* Website title linking to homepage */}
+                <p className="header-title">BOUND BY BORDERS</p>
+                </ Link>
+            </div>
+
+            <div className="header-pages">
+                <Link to="/about"> 
+                    <p >About</p>
+                </Link>
+            
+                <Link to="/survey"> 
+                    <p>Survey</p>
+                </Link>
+            </div>
+        </header>
   )
 }
