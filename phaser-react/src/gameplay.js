16286a58a124bb53aa5b33c5dd414d54b4c07a15
My Tuyen Phan <mytuyenp@gmail.com>
2024-11-11 02:30:05 -0600

    added student_visa_route (parent state) with all subsequet children states

--- phaser-react/src/gameplay.js
+++ phaser-react/src/gameplay.js
@@ -1,235 +1,346 @@
 import { createMachine, assign } from "xstate";
 export const gameplayMachine = createMachine(
-  {
-    context: {
-      wealth: "2000",
-      visaType: null,
-      canAffordUni: false,
-      fluentInEnglish: false,
-      canAffordClasses: false,
-    },
-    id: "gameplay",
-    initial: "catalyst_earth",
-    states: {
-      catalyst_earth: {
-        on: {
-          CONTINUE: [
-            {
-              target: "catalyst_country",
-              actions: [],
-            },
-          ],
+    {
+        context: {
+            wealth: "2000",
+            visaType: null,
+            canAffordUni: false,
+            fluentInEnglish: false,
+            canAffordClasses: false,
         },
-      },
-      catalyst_country: {
-        on: {
-          CONTINUE: [
-            {
-              target: "objective",
-              actions: [],
-            },
-          ],
-        },
-      },
-      objective: {
-        on: {
-          ILLEGAL_OPTION_SELECTED: [
-            {
-              target: "illegal_route",
-              actions: [],
-            },
-          ],
-          LEGAL_OPTION_SELECTED: [
-            {
-              target: "legal_route",
-              actions: [],
-            },
-          ],
-        },
-      },
-      illegal_route: {
-        initial: "find_coyote",
+        id: "gameplay",
+        initial: "catalyst_earth",
         states: {
-          find_coyote: {
-            on: {
-              CONTINUE: [
-                {
-                  target: "cost_of_coyote",
-                  actions: [],
+            catalyst_earth: {
+                on: {
+                    CONTINUE: [
+                        {
+                            target: "catalyst_country",
+                            actions: [],
+                        },
+                    ],
                 },
-              ],
             },
-          },
-          cost_of_coyote: {
-            on: {
-              CONTINUE: [
-                {
-                  target: "enough_money",
-                  actions: [],
+            catalyst_country: {
+                on: {
+                    CONTINUE: [
+                        {
+                            target: "objective",
+                            actions: [],
+                        },
+                    ],
                 },
-              ],
             },
-          },
-          enough_money: {
-            on: {
-              CONTINUE: [
-                {
-                  target: "days_pass",
-                  actions: [],
+            objective: {
+                on: {
+                    ILLEGAL_OPTION_SELECTED: [
+                        {
+                            target: "illegal_route",
+                            actions: [],
+                        },
+                    ],
+                    LEGAL_OPTION_SELECTED: [
+                        {
+                            target: "legal_route",
+                            actions: [],
+                        },
+                    ],
                 },
-              ],
             },
-          },
-          days_pass: {
-            on: {
-              CONTINUE: [
-                {
-                  target: "desert",
-                  actions: [],
+            illegal_route: {
+                initial: "find_coyote",
+                states: {
+                    find_coyote: {
+                        on: {
+                            CONTINUE: [
+                                {
+                                    target: "cost_of_coyote",
+                                    actions: [],
+                                },
+                            ],
+                        },
+                    },
+                    cost_of_coyote: {
+                        on: {
+                            CONTINUE: [
+                                {
+                                    target: "enough_money",
+                                    actions: [],
+                                },
+                            ],
+                        },
+                    },
+                    enough_money: {
+                        on: {
+                            CONTINUE: [
+                                {
+                                    target: "days_pass",
+                                    actions: [],
+                                },
+                            ],
+                        },
+                    },
+                    days_pass: {
+                        on: {
+                            CONTINUE: [
+                                {
+                                    target: "desert",
+                                    actions: [],
+                                },
+                            ],
+                        },
+                    },
+                    desert: {
+                        on: {
+                            CONTINUE: [
+                                {
+                                    target: "death",
+                                    actions: [],
+                                },
+                            ],
+                        },
+                    },
+                    death: {
+                        on: {
+                            CONTINUE: [
+                                {
+                                    target: "failed_objective",
+                                    actions: [],
+                                },
+                            ],
+                        },
+                    },
+                    failed_objective: {},
                 },
-              ],
             },
-          },
-          desert: {
-            on: {
-              CONTINUE: [
-                {
-                  target: "death",
-                  actions: [],
+            legal_route: {
+                initial: "choose_visas",
+                states: {
+                    choose_visas: {
+                        on: {
+                            STUDENT: [
+                                {
+                                    target: "#gameplay.student_visa_route",
+                                    actions: [
+                                        {
+                                            type: "assignVisaType",
+                                        },
+                                    ],
+                                },
+                            ],
+                            DIVERSITY: [
+                                {
+                                    target: "ineligible",
+                                    actions: [
+                                        {
+                                            type: "assignVisaType",
+                                        },
+                                    ],
+                                },
+                            ],
+                            FIANCE: [
+                                {
+                                    target: "ineligible",
+                                    actions: [
+                                        {
+                                            type: "assignVisaType",
+                                        },
+                                    ],
+                                },
+                            ],
+                            FAMILY: [
+                                {
+                                    target: "ineligible",
+                                    actions: [
+                                        {
+                                            type: "assignVisaType",
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                    },
+                    ineligible: {
+                        on: {
+                            CHOOSE_OTHER_VISA: [
+                                {
+                                    target: "choose_visas",
+                                    actions: [
+                                        {
+                                            type: "resetVisaType",
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                    },
                 },
-              ],
             },
-          },
-          death: {
-            on: {
-              CONTINUE: [
-                {
-                  target: "failed_objective",
-                  actions: [],
+            student_visa_route: {
+                initial: "insufficient",
+                states: {
+                    insufficient: {
+                        on: {
+                            LEARN_ENGLISH: [
+                                {
+                                    target: "learn_english",
+                                    actions: [],
+                                },
+                            ],
+                            GET_A_JOB: [
+                                {
+                                    target: "jobs_available",
+                                    actions: [],
+                                },
+                            ],
+                        },
+                    },
+                    learn_english: {
+                        on: {
+                            LEARN_ALONE: [
+                                {
+                                    target: "fluent",
+                                    actions: [],
+                                },
+                            ],
+                            ENROLL_CLASS: [
+                                {
+                                    target: "broke",
+                                    actions: [],
+                                },
+                            ],
+                        },
+                    },
+                    jobs_available: {
+                        on: {
+                            CALL_CENTER: [
+                                {
+                                    target: "requires_english",
+                                    actions: [],
+                                },
+                            ],
+                            CARNICERIA: [
+                                {
+                                    target: "can_afford_uni",
+                                    actions: [],
+                                },
+                            ],
+                        },
+                    },
+                    fluent: {
+                        entry: {
+                            type: "fluentInEnglish",
+                        },
+                        on: {
+                            EARN_MONEY: [
+                                {
+                                    target: "jobs_available",
+                                    actions: [],
+                                },
+                            ],
+                            CONTINUE: [
+                                {
+                                    target: "#gameplay.completed_objective",
+                                    guard: "qualified",
+                                    actions: [],
+                                },
+                            ],
+                        },
+                    },
+                    broke: {
+                        on: {
+                            LEARN_ALONE: [
+                                {
+                                    target: "fluent",
+                                    actions: [],
+                                },
+                            ],
+                        },
+                    },
+                    requires_english: {
+                        on: {
+                            CARNICERIA: [
+                                {
+                                    target: "can_Afford_Uni",
+                                    actions: [],
+                                },
+                            ],
+                        },
+                    },
+                    can_Afford_Uni: {
+                        entry: {
+                            type: "canAffordUni",
+                        },
+                        on: {
+                            LEARN_ENGLISH: [
+                                {
+                                    target: "learn_english",
+                                    actions: [],
+                                },
+                            ],
+                            CONTINUE: [
+                                {
+                                    target: "#gameplay.completed_objective",
+                                    guard: "qualified",
+                                    actions: [],
+                                },
+                            ],
+                        },
+                    },
                 },
-              ],
             },
-          },
-          failed_objective: {},
+            completed_objective: {
+              type: "final",
+            },
         },
-      },
-      legal_route: {
-        initial: "choose_visas",
-        states: {
-          choose_visas: {
-            on: {
-              STUDENT: [
+        on: {
+            RESTART: [
                 {
-                  target: "#gameplay.student_visa_route",
-                  actions: [
-                    {
-                      type: "assignVisaType",
-                    },
-                  ],
+                    target: ".objective",
+                    actions: [
+                        {
+                            type: "resetGame",
+                        },
+                    ],
                 },
-              ],
-              DIVERSITY: [
-                {
-                  target: "ineligible",
-                  actions: [
-                    {
-                      type: "assignVisaType",
-                    },
-                  ],
-                },
-              ],
-              FIANCE: [
-                {
-                  target: "ineligible",
-                  actions: [
-                    {
-                      type: "assignVisaType",
-                    },
-                  ],
-                },
-              ],
-              FAMILY: [
-                {
-                  target: "ineligible",
-                  actions: [
-                    {
-                      type: "assignVisaType",
-                    },
-                  ],
-                },
-              ],
-            },
-          },
-          ineligible: {
-            on: {
-              CHOOSE_OTHER_VISA: [
-                {
-                  target: "choose_visas",
-                  actions: [
-                    {
-                      type: "resetVisaType",
-                    },
-                  ],
-                },
-              ],
-            },
-          },
+            ],
         },
-      },
-      student_visa_route: {
-        initial: "insufficient",
-        states: {
-          insufficient: {
-            on: {
-              "LEARN ENGLISH": [
-                {
-                  target: "New state 1",
-                  actions: [],
-                },
-              ],
-              "GET A JOB": [
-                {
-                  target: "New state 1",
-                  actions: [],
-                },
-              ],
-            },
-          },
-          "New state 1": {},
-        },
-      },
     },
-    on: {
-      RESTART: [
-        {
-          target: ".objective",
-          actions: [
-            {
-              type: "resetGame",
-            },
-          ],
+    {
+        actions: {
+            resetGame: assign(({ initialContext }) => {
+                return initialContext; // Resets the entire context to its initial state
+            }),
+            resetVisaType: assign(() => {
+                return {
+                    visaType: null,
+                };
+            }),
+            assignVisaType: assign(({ context, event }) => {
+                return {
+                    visaType: event.type, // Ensure event type is passed correctly
+                };
+            }),
+            canAffordUni: assign(({ context }) => {
+                return {
+                    ...context,
+                    canAffordUni: true,
+                };
+            }),
+            fluentInEnglish: assign(({ context }) => {
+                return {
+                    ...context,
+                    fluentInEnglish: true,
+                };
+            }),
         },
-      ],
-    },
-  },
-  {
-    actions: {
-      resetGame: assign(({ initialContext }) => {
-        return initialContext; // Resets the entire context to its initial state
-      }),
-      resetVisaType: assign(() => {
-        return {
-          visaType: null,
-        };
-      }),
-      assignVisaType: assign(({ context, event }) => {
-        return {
-          visaType: event.type, // Ensure event type is passed correctly
-        };
-      }),
-    },
-    actors: {},
-    guards: {},
-    delays: {},
-  },
+        actors: {},
+        guards: {
+            qualified: (context) => {
+                return context.canAffordUni && context.fluentInEnglish;
+            },
+        },
+        delays: {},
+    }
 );
+
