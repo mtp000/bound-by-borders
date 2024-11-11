0e5b754febdb4b6f4804f505d382146f2fa33256
My Tuyen Phan <mytuyenp@gmail.com>
2024-11-11 06:27:04 -0600

    add description

--- phaser-react/src/states/flow2/student/CompletedObjective.jsx
+++ phaser-react/src/states/flow2/student/CompletedObjective.jsx
@@ -2,6 +2,23 @@ import React from 'react'
 
 export default function CompletedObjective({ send }) {
   return (
-    <div>Completed Objective</div>
+    <>
+      <h1>Completed Objective</h1>
+      <p>
+        Congratulations on successfuly making it to the US! <br/><br/>
+        As a student visa holder in the U.S., you are permitted to:
+        <ul>
+          <li>Study at an accredited U.S. institution</li>
+          <li>Work part-time on campus (up to 20 hours per week during the semester, full-time during breaks)</li>  
+          <li>Travel in and out of the U.S. with proper documentation</li>
+        </ul> 
+
+        What Student Visa Holders Are Not Allowed to Do:
+        <ul>
+          <li>Work off-campus</li>
+          <li>Stay in the U.S. after the visa expires without proper extension or applying for work authorization</li>
+        </ul>
+      </p>
+    </>
   )
 }
